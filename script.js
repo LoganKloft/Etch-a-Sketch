afcd8c103065e0da8f1ad57d0a0d2f5015aed2a1
Logan Kloft <lkloft@outlook.com>
2022-07-27 16:28:33 -0700

    Add pen effect to grid

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+let penColor = 'black';
 
 // Create 16 rows and add 16 squares to each
 function CreateGrid(container) {
@@ -8,10 +9,15 @@ function CreateGrid(container) {
         for (let j = 0; j < 16; j++) {
             let square = document.createElement('div');
             square.classList.add('square');
+            square.addEventListener('mouseover', ChangeColorHandler);
             row.appendChild(square);
         }
         container.appendChild(row);
     }
+}
+
+function ChangeColorHandler() {
+    this.style.backgroundColor = 'black';
 }
 
 CreateGrid(container);
